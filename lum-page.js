10b545ce0a3714c8294b19d05d3ae6c5bccbc2bb
Joseph Lum <lum-joseph@noreply.users.github.com>
2023-11-30 15:21:35 +0800

    updated the javascript file and css styles to change introduction sectino to paralell scroll instead of fade in & fade out.

--- lum-page.js
+++ lum-page.js
@@ -1,33 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the section element
-    var section = document.querySelector(".fade-in-section");
-
-    // Initialize variables to track scroll direction and last scroll position
-    var lastScrollTop = 0;
-    var isScrollingDown = true;
-
-    // Add a scroll event listener
-    window.addEventListener("scroll", function () {
-        // Get the current scroll position
-        var currentScrollTop = window.scrollY;
-
-        // Check the scroll direction
-        isScrollingDown = currentScrollTop > lastScrollTop;
-
-        // Check if the section is in the viewport
-        var sectionRect = section.getBoundingClientRect();
-        var isVisible = sectionRect.top < window.innerHeight && sectionRect.bottom >= 0;
-
-        // Toggle the "fade-in" class based on scroll direction and visibility
-        if (isVisible) {
-            if (isScrollingDown) {
-                section.classList.add("fade-in");
-            } else {
-                section.classList.remove("fade-in");
-            }
-        }
-
-        // Update the last scroll position
-        lastScrollTop = currentScrollTop;
-    });
+// Parallax scrolling effect
+document.addEventListener('scroll', function () {
+    var scrollTop = window.scrollY;
+    var parallaxSection = document.querySelector('.parallax-section');
+    parallaxSection.style.backgroundPositionY = -scrollTop * 0.3 + 'px'; // Adjust the multiplier for the parallax effect
 });
